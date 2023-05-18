42ba0de93ab3518c172c25dfb5a30aae3327275d
= <jackocratere@gmail.com>
2023-05-18 23:50:22 +0200

    Création du formulaire User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,16 +4,51 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use App\Form\UserType;
+
+use Doctrine\ORM\EntityManagerInterface;
 
 class UserController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+    
     #[Route('/user', name: 'app_user')]
     public function index(): Response
     {
+        $users = $this->entityManager->getRepository(User::class)->findAll();
+
         return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+            // 'controller_name' => 'UserController',
+            'users' => $users,
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->entityManager->persist($user);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('user_show', [
+                'id' => $user->getId(),
+            ]);
+        }
+
+        return $this->render('user/create.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
     // public function index(Request $request): Response
