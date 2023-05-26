0520866c06d011e898d34bf78dec4aba3c2b6051
= <jackocratere@gmail.com>
2023-05-26 02:40:49 +0200

    Création de la page d'accueil + du header

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,63 +2,77 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-use Doctrine\ORM\EntityManagerInterface;
-
+#[Route('/user')]
 class UserController extends AbstractController
 {
-    private $entityManager;
-
-    public function __construct(EntityManagerInterface $entityManager)
+    #[Route('/', name: 'app_user_index', methods: ['GET'])]
+    public function index(UserRepository $userRepository): Response
     {
-        $this->entityManager = $entityManager;
-    }
-    
-    #[Route('/user', name: 'app_user')]
-    public function index(): Response
-    {
-        $users = $this->entityManager->getRepository(User::class)->findAll();
-
         return $this->render('user/index.html.twig', [
-            // 'controller_name' => 'UserController',
-            'users' => $users,
+            'users' => $userRepository->findAll(),
         ]);
     }
 
-    public function create(Request $request)
+    #[Route('/new', name: 'app_user_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, UserRepository $userRepository): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid())
-        {
-            $this->entityManager->persist($user);
-            $this->entityManager->flush();
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
 
-            return $this->redirectToRoute('user_show', [
-                'id' => $user->getId(),
-            ]);
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('user/create.html.twig', [
-            'form' => $form->createView(),
+        return $this->renderForm('user/new.html.twig', [
+            'user' => $user,
+            'form' => $form,
         ]);
     }
-    // public function index(Request $request): Response
-    // {
-    //     $greet = '';
-    //     if ($name = $request->query->get('hello')) {
-    //         $greet = sprintf('<h1>Hello %s!</h1>', htmlspecialchars($name));
-    //     } 
-    //     return $this->render('user/index.html.twig', [
-    //         'controller_name' => 'UserController',
-    //     ]);
-    // }
+
+    #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $userRepository->save($user, true);
+
+            return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('user/edit.html.twig', [
+            'user' => $user,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_user_delete', methods: ['POST'])]
+    public function delete(Request $request, User $user, UserRepository $userRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->request->get('_token'))) {
+            $userRepository->remove($user, true);
+        }
+
+        return $this->redirectToRoute('app_user_index', [], Response::HTTP_SEE_OTHER);
+    }
 }
